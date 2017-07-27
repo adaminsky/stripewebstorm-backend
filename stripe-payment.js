ad738707315741c9988294d4191532037792d552
edmond8tu <edmondtu@students.berkeley.net>
2017-07-27 21:45:06 +0800

    firebase handling

--- stripe-payment.js
+++ stripe-payment.js
@@ -4,17 +4,6 @@ import express from 'express';
 import { fromExpress } from 'webtask-tools';
 import bodyParser from 'body-parser';
 import stripe from 'stripe';
-var firebase = require('firebase');
-
-var config = {
-    apiKey: "AIzaSyAI0hjtt6DpgtboWpyOcWsh_Xn6BxVYjMY",
-    authDomain: "bobago-c7da5.firebaseapp.com",
-    databaseURL: "https://bobago-c7da5.firebaseio.com",
-    projectId: "bobago-c7da5",
-    Storagebucket: "bobago-c7da5.appspot.com",
-    messagingSenderId: "246035056974"
-};
-firebase.initializeApp(config);
 
 
 bodyParser.urlencoded();
